ae166f581d79995333ea00f61eb27ebc11754b56
Cathaspa <cathaspa@protonmail.com>
2017-07-04 17:33:34 -0400

    add the beginnings of a CLI interface. It currently accepts correct args but does nothing with them.

---
+++ helix.py
@@ -0,0 +1,31 @@
+'''
+Copyright (c) 2017 Anne-Laure Ehresmann
+Licenced under the MIT License (https://en.wikipedia.org/wiki/MIT_License)
+helix.py - the CLI for easy employment of various automisation scripts.
+
+Usage:
+helix.py group <group_input> 
+helix.py graph <graph_input> [--heat] [--scatter] <control> <sample> [<sample> ...]
+helix.py group <group_input> graph <graph_input> [--heat] [--scatter] <control> <sample> [<sample> ...]
+helix.py (-h | --help)
+
+The control and samples can either be column names, either column indexes.
+In the graphing options, if you specify neither --scatter nor --heat, it will be assumed you want both.
+
+Examples:
+helix.py group input.csv
+helix.py graph output.csv heat 1 log_fc_2,log_fc_3,log_fc_4
+helix.py group input.csv graph output.csv scatter 1,2 log_fc_3,4 5,log_fc_6
+
+Options:
+-h --help       prints this message and exit
+-v              verbose mode
+-q		quiet mode
+
+'''
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    print(arguments)
