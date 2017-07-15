9adca3d248f6f91e40f696c3237c4f672311649a
Cathaspa <cathaspa@protonmail.com>
2017-07-15 16:48:50 -0400

    graph_genes broken, but now have communicaton between helixpc and group_genes. bunch of stuff added for safe packaging.

--- helixpc/graph_genes.py
+++ helixpc/graph_genes.py
@@ -1,8 +1,3 @@
-''' Copyright (c) 2017 Anne-Laure Ehresmann
-Licenced under the MIT License (https://en.wikipedia.org/wiki/MIT_License)
-'''
-# TODO: Make labels work for mean, figure out bug when receiving more than one
-# sample
 import sys
 import numpy as np
 import pandas as pd
@@ -13,8 +8,6 @@ from plotly.offline import plot
 
 def verify_inputs(inpt_arr, headers):
     cols = []
-    if (len(inpt_arr) == 1) & (inpt_arr[0] == 'end'):
-        return 'end'
     for inpt in inpt_arr:
         if not inpt.isdigit():
             for j in range(len(headers)):
@@ -24,74 +17,16 @@ def verify_inputs(inpt_arr, headers):
                     valid = True
                     break
             if not valid:
-                print('your input '%s' was invalid! E0' % (inpt))
-                return None
+                print('your input \'%s\' was invalid! E0' % (inpt))
+                sys.exit()
         elif (int(inpt) > 1) & (int(inpt) <= len(headers)):
                 cols.append(inpt)
         else:
-            print('your input '%s' was invalid! E1' % (inpt))
-            return None
+            print('your input \'%s\' was invalid! E1' % (inpt))
+            sys.exit()
     for i in range(len(cols)):
         cols[i] = int(cols[i]) - 1
     return cols
-
-
-def ask_for(query, headers):
-    valid = False
-    while not valid:
-        valid = True
-        inpt_arr = input(query).split()
-        cols = verify_inputs(inpt_arr, headers)
-        if cols is None:
-            valid = False
-            print('\nRemember, you can logically not give the first' +
-                  'row (gene names) as an input. Make sure to spell ' +
-                  'your column names correctly!\n')
-    return cols
-
-
-def gen_index_array(data):
-    output = []
-    controls = ask_for('\nAsking for Control(s). Which column(s) are '
-                       'the control(s)? \nEnter them ' +
-                       'with a space in between each. \nYou may enter ' +
-                       'their name (log_fc...) or their number index ' +
-                       '(2, 3, ....).\n> ', list(data))
-    if controls == 'end':
-        print('Invalid Control. Exiting.')
-        sys.exit()
-
-    output.append(controls)
-    more = True
-    while more:
-        query = ('\nAsking for sample %d. Which ' % (len(output)) +
-                 'column(s) are in the sample? enter 'end' to stop ' +
-                 'entering samples.\n> ')
-        sample = ask_for(query, list(data))
-        if sample == 'end':
-            more = False
-        else:
-            output.append(sample)
-
-    if len(output) == 1:
-        print('No samples. Exiting.')
-        sys.exit()
-    return output
-
-
-def gen_graph_array(data, index_arr):
-    output = []  # array of DataFrames
-    for i in range(len(index_arr)):
-        sample_frame = pd.DataFrame()
-        for index in index_arr[i]:
-            sample_frame[str(list(data)[i+1])] = data.iloc[:, index].values
-        if len(sample_frame.columns) > 1:
-            sample_frame = sample_frame.mean(axis=1)
-        name = list(sample_frame)[0]
-        sample_frame = sample_frame.squeeze()
-        sample_frame.rename(name)
-        output.append(sample_frame.squeeze())  # Append only series
-    return output
 
 
 def gen_graphs(samples, gene_names):
@@ -126,11 +61,16 @@ def remove_na_rows(series_arr, gene_names):
     return(output)
 
 
-if len(sys.argv) != 2:
-    print('You must give me (at-least / only) one source file. Exiting.')
-    sys.exit()
-
-data = pd.read_csv(sys.argv[1], skipinitialspace=True)
-index_arr = gen_index_array(data)
-graph_arr = gen_graph_array(data, index_arr)
-gen_graphs(graph_arr, np.array(data.iloc[:, 0].values).tolist())
+# master function
+def input(inputfile, scatter, heat, control, samples):
+    print(samples)
+    data = pd.read_csv(inputfile, skipinitialspace=True)
+    index_arr = []  # array of DataFrames
+    control_arr = verify_inputs(control.split(','), list(data))
+    samples_arr =[]
+    for sample in samples:
+        validsamp = verify_inputs(sample.split(','), list(data))
+        samples_arr.append(validsamp)
+    index_arr = [control_arr, samples_arr]
+    print(index_arr)
+    # gen_graphs(graph_arr, np.array(data.iloc[:, 0].values).tolist())
--- helixpc/group_genes.py
+++ helixpc/group_genes.py
@@ -1,8 +1,3 @@
-'''
-Copyright (c) 2017 Anne-Laure Ehresmann
-Licenced under the MIT License (https://en.wikipedia.org/wiki/MIT_License)
-'''
-
 import sys
 import pandas as pd
 
@@ -34,37 +29,35 @@ def capitalise(sample):
     return pd.concat([samp_series, sample.iloc[:, 1:len(sample)]], axis=1)
 
 
-if (len(sys.argv) < 2):
-    print('You must give me (at-least / only) one source file. Exiting.')
-    sys.exit()
-samples = build_samples(sys.argv[1])
-
-if len(sys.argv) < 3 or (sys.argv[2] != 'y'):
-    print('Capitalising genes... Note that if your input file ' +
-          'already contains only capital letters, and is sorted, you ' +
-          'can pass \'y\' after passing your input file to skip this step.')
-    for i in range(len(samples)):
-        samples[i] = capitalise(samples[i])
-    print('Sorting...')
-    for i in range(len(samples)):
-        samples[i] = samples[i].sort_values([list(samples[i])[0]])
-
-
 def get_mean_of_dups(sample):
     sample = sample.groupby('gene_symbol').mean().reset_index()
     return sample
 
+# master function
+def input(inputfile, output, nonan, sort):
+    print(output)
+    print('Building database structure...')
+    samples = build_samples(inputfile)
+    if  not sort:
+        print('Capitalising genes...')
+        for i in range(len(samples)):
+            samples[i] = capitalise(samples[i])
+        print('Sorting...')
+        for i in range(len(samples)):
+            samples[i] = samples[i].sort_values([list(samples[i])[0]])
 
-all = samples[0].copy(deep=True)
-all = get_mean_of_dups(all)
-for i in range(1, len(samples)):
-    s = samples[i]
-    if 'gene_symbol' in s.columns[0]:
-        cols = s.columns.values
-        cols[0] = 'gene_symbol'
-        s.columns = cols
-    s = get_mean_of_dups(s)
-    print(type(s))
-    all = all.merge(s, how='outer', on='gene_symbol', copy=False)
-print(all)
-all.to_csv('output.csv', index=False)
+    all = samples[0].copy(deep=True)
+    all = get_mean_of_dups(all)
+    for i in range(1, len(samples)):
+        s = samples[i]
+        if 'gene_symbol' in s.columns[0]:
+            cols = s.columns.values
+            cols[0] = 'gene_symbol'
+            s.columns = cols
+        s = get_mean_of_dups(s)
+        all = all.merge(s, how='outer', on='gene_symbol', copy=False)
+    all = all.sort_values([list(all)[0]])
+    if nonan:
+        print('Excluding rows with missing entries...')
+        all = all.dropna()
+    all.to_csv((output+'.csv'), index=False)
